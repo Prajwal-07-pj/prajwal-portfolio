26229604f22bef79c9121076d569152b7cb8d784
Prajwal Jadhav <jadhavprajwal2307@gmail.com>
2026-03-30 23:10:06 +0530

    change in front image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,18 +37,22 @@ const Hero = () => {
         </div>
 
         <div className="flex h-full items-center justify-center">
-          <div className="relative aspect-[4/5] w-[min(92%,760px)] border-l border-t border-white/10 bg-[linear-gradient(160deg,rgba(8,8,8,0.95),rgba(2,2,2,0.96))] max-lg:w-[min(95%,560px)] max-lg:aspect-[4/4.8]">
+          <div className="relative aspect-[4/5] w-[min(92%,760px)] overflow-hidden border-l border-t border-white/10 bg-[linear-gradient(160deg,rgba(8,8,8,0.95),rgba(2,2,2,0.96))] max-lg:w-[min(95%,560px)] max-lg:aspect-[4/4.8]">
             <img src={prajwalImage} alt="Prajwal portrait" className="h-full w-full scale-[0.9] object-cover object-[center_35%] [filter:contrast(1.08)_saturate(0.55)_brightness(0.86)_drop-shadow(0_0_10px_rgba(255,255,255,0.14))_drop-shadow(0_10px_20px_rgba(0,0,0,0.32))] md:scale-[0.94]"/>
             <div className="pointer-events-none absolute left-[21%] top-[10%] h-[36%] w-[52%] rotate-[-14deg] border border-white/20" aria-hidden="true"></div>
             <div className="pointer-events-none absolute left-[33%] top-[27%] h-[42%] w-[60%] rotate-[18deg] border border-white/20" aria-hidden="true"></div>
             <div className="pointer-events-none absolute left-[25%] top-[58%] h-[28%] w-[48%] rotate-[-8deg] border border-white/20" aria-hidden="true"></div>
 
-            <div className="absolute bottom-[18px] left-1/2 flex h-[44px] w-[27px] -translate-x-1/2 justify-center rounded-[15px] border border-[#ff5f1f] bg-[#040404]/65 pt-2" aria-hidden="true">
+            <div className="pointer-events-none absolute inset-x-0 bottom-0 z-10 h-24 bg-gradient-to-b from-transparent via-[#050505]/85 to-[#060606]" aria-hidden="true"></div>
+
+            <div className="absolute bottom-[18px] left-1/2 z-20 flex h-[44px] w-[27px] -translate-x-1/2 justify-center rounded-[15px] border border-[#ff5f1f] bg-[#040404]/65 pt-2" aria-hidden="true">
               <span className="h-[10px] w-1 animate-bounce rounded-[10px] bg-[#ff5f1f]"></span>
             </div>
           </div>
         </div>
       </div>
+
+      <div className="pointer-events-none absolute inset-x-0 bottom-0 h-20 bg-gradient-to-b from-transparent via-[#050505] to-[#080808]" aria-hidden="true"></div>
     </section>);
 };
 export default Hero;
